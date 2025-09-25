ae0b3c1d0bb6083addcb7ed121bcd8ff425915ca
leestana01 <leestana01@naver.com>
2025-09-25 16:44:27 +0900

    feat: swagger https 강제

--- src/main/java/goormthon/jeju/global/config/SwaggerConfig.java
+++ src/main/java/goormthon/jeju/global/config/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +15,7 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(new Info()
                         .title("제주 병원 셔틀 API")
                         .description("제주 병원 셔틀 예약 시스템 API 문서")
@@ -25,5 +27,11 @@ public class SwaggerConfig {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
+
+        openAPI.addServersItem(new Server()
+                .url("https://api-donghang.klr.kr")
+                .description("Production HTTPS Server"));
+
+        return openAPI;
     }
 }
